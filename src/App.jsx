93d8fed78d1af976203668261c998accd02ee558
Vladislav <vlad11ponomarev@gmail.com>
2024-07-23 22:47:31 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,15 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { Services } from "./pages/Services/Services";
-import { useState, useEffect, CSSProperties } from "react";
-import ClipLoader from "react-spinners/ClipLoader";
 
-const override: CSSProperties = {
-  display: "block",
-  margiinTop: '50%',
-  margin: "0 auto",
-  borderColor: "#006400",
-  color: "brown",
-};
+
+
 
 export const App = () => {
 
-  const [loading, setLoading] = useState(false);
-   let [color, setColor] = useState("#006400");
-  
-  useEffect(() => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 300)
-  },[])
-
   return (
    <>
-    {
-      loading ? 
-        
-      <ClipLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader" />
-          :
+  
       <Routes>
         <Route path='/' element={<Layout />}>
         <Route index path="/" element={<HomePage />} />
@@ -45,7 +18,7 @@ export const App = () => {
           <Route path="*" element={<Navigate replace to="/" />} />
         </Route> 
       </Routes> 
-    }
+    
   </>
   );
   
